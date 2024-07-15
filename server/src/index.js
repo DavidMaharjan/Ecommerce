417a3c07c15ad11620bb5185b30e88be3ec2c2ea
David Maharjan <dmaharjan224@gmail.com>
2024-07-15 17:48:30 +0545

    route setup for register

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,10 @@
 const express = require('express')
 const app = express()
 const port = 8000
+const userRoute=require("./routes/users.js")
+app.use(userRoute)
 
-app.get('/me', (req, res) => {
-  res.send('Hello World!')
-})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
